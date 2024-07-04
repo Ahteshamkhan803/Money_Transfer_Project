f9e73b88f24d6531ef3e23c6c9012e9fa324213f
AhteshamKhan803 <ahteshamkhangit803@gmail.com>
2024-07-04 15:38:58 +0530

    changes

--- MoneyTransfer_Project/settings.py
+++ MoneyTransfer_Project/settings.py
@@ -90,10 +90,23 @@ WSGI_APPLICATION = 'MoneyTransfer_Project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'money_transfer_project',  
+        'USER': 'root',  
+        'PASSWORD': 'Ahtesham@123',  
+        'HOST': 'localhost',  
+        'PORT': '3306',  
     }
 }
 
@@ -151,7 +164,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'bekaarhai803@gmail.com'
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_PASSWORD = 'bnevqpbajnvbyxcg'
 
 
 ##############################################################
